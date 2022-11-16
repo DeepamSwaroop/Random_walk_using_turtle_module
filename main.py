fb2bc50de8b571a70ffe6f83fb9fcf76779a04f9
Deepam Swaroop <swaroopdeepam5@gmail.com>
2022-11-16 10:38:41 +0530

    Used colormode function

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import turtle
 from turtle import Turtle,Screen
 import random
 t=Turtle()
 t.pensize(15)
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed",
-           "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+turtle.colormode(255)
 direction=[0,90,180,270]
 t.speed(9)
+def color():
+    r=random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color=(r,g,b)
+    return random_color
 for i in range(200):
     t.forward(30)
     t.setheading(random.choice(direction))
 
-    t.color(random.choice(colours))
+    t.color(color())
 
 
 
